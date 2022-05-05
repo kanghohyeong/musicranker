faa9436906a0628621fcc22d9b4447e22964e76c
kanghohyeong <hobro.kang@gmail.com>
2022-05-05 14:57:34 +0900

    init project

---
+++ src/main/kotlin/org/hobro/musicranker/MusicRankerApplication.kt
@@ -0,0 +1,11 @@
+package org.hobro.musicranker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MusicRankerApplication
+
+fun main(args: Array<String>) {
+    runApplication<MusicRankerApplication>(*args)
+}
---
+++ src/test/kotlin/org/hobro/musicranker/MusicRankerApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.hobro.musicranker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MusicRankerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
